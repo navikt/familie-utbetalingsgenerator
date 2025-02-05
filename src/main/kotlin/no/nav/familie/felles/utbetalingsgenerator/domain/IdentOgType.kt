e7b411b1d92a814aa4513555f30d2320c585a361
bragejahren <brage.ekroll.jahren@nav.no>
2025-02-05 10:27:40 +0100

    Skriver enkel test for å validere sortering av IdentOgType

--- src/main/kotlin/no/nav/familie/felles/utbetalingsgenerator/domain/IdentOgType.kt
+++ src/main/kotlin/no/nav/familie/felles/utbetalingsgenerator/domain/IdentOgType.kt
@@ -4,7 +4,5 @@ data class IdentOgType(
     val ident: String,
     val type: Ytelsestype,
 ) : Comparable<IdentOgType> {
-    override fun compareTo(other: IdentOgType): Int {
-        return (type.klassifisering + ident).compareTo(other.type.klassifisering  + other.ident)
-    }
+    override fun compareTo(other: IdentOgType): Int = (type.klassifisering + ident).compareTo((other.type.klassifisering + other.ident))
 }
